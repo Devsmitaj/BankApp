6d76e1e1248d20f7bcc3fea14210fb6f56f697ff
Devsmita <devsmita.jagtap@gmail.com>
2018-05-26 22:09:16 -0700

    First comit

---
+++ BankApp/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    /// <summary>
+    /// Bank account
+    /// </summary>
+    class Account
+    {
+        #region
+        /// <summary>
+        /// Unique account number for the account
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Email address of 
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// Account Current Balance
+        /// </summary>
+        public decimal Balance { get; set; }
+
+        public string TypeOfAccount { get; set; }
+        #endregion
+
+    }
+}
